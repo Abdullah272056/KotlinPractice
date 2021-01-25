e1dfa3d1039ef870d552aa28c1c914a20ecd3efb
Abdullah272056 <abdullah272056@gmail.com>
2021-01-25 14:17:08 +0600

    intent set

--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -2,10 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val  button=findViewById<Button>(R.id.buttonId);
+        button.setOnClickListener {
+            Toast.makeText(this,"click",Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -2,13 +2,25 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity3 extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        button=findViewById(R.id.buttonId);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              
+                Toast.makeText(MainActivity3.this, "clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
